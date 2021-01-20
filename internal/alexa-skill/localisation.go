2aafdce1f54d7c6355bbe8791b2f1d3ccef91267
Antoine Crochet <acrochet95@gmail.com>
2021-01-20 23:17:27 +0100

    Add space after upcomping bus messages (weird sentence by Alexa due to concatenation)

--- internal/alexa-skill/localisation.go
+++ internal/alexa-skill/localisation.go
@@ -5,8 +5,8 @@ import "gitlab.com/dasjott/alexa-sdk-go"
 var Locales = alexa.Localisation{
 	"fr-FR": alexa.Translation{
 		"WELCOME_MSG":            "Bienvenue sur transport rennes! Que voulez vous savoir ?",
-		"UPCOMING_ONE_BUS_MSG":   "Le {bus}, arrêt {busstop}, direction {destination}: prochain départ dans {dep1} minutes.",
-		"UPCOMING_TWO_BUSES_MSG": "Le {bus}, arrêt {busstop}, direction {destination}: prochain départ dans {dep1} minutes, le suivant dans {dep2} minutes.",
+		"UPCOMING_ONE_BUS_MSG":   "Le {bus}, arrêt {busstop}, direction {destination}: prochain départ dans {dep1} minutes. ",
+		"UPCOMING_TWO_BUSES_MSG": "Le {bus}, arrêt {busstop}, direction {destination}: prochain départ dans {dep1} minutes, le suivant dans {dep2} minutes. ",
 		"NO_BUS_AVAILABLE":       "Aucun bus n'est actuellement prévu",
 		"HELP_MSG":               "Je peux vous indiquer quand arrive le prochain bus. Dites simplement 'quand arrive le prochain bus'.",
 		"GOODBYE_MSG":            "Au revoir!",
